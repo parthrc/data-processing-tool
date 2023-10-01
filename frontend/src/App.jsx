74f4867f934ecc34428f5547678635be5140c0fa
unknown <parthrc00@gmail.com>
2023-10-01 19:17:57 +0530

    Pushed to github

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,15 @@
 import Homepage from "./Pages/Homepage/Homepage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <Homepage />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Homepage />} />
+        <Route path="login" element={<Homepage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
